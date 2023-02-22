8854e05fcab9c3900f3851abe3c0ec33c24262f6
Robin Neatherway <rneatherway@github.com>
2023-02-22 12:11:47 +0000

    Add repository and clean context

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,27 @@ import * as inputs from "./inputs";
 
 const preamble = `
 #!/usr/bin/env python3
-from github import Github
-import os
 
-token = os.environ["GITHUB_TOKEN"]
-github = Github(token)
+def setup():
+	from github import Github
+	import os
+
+	token = os.environ["GITHUB_TOKEN"]
+	github = Github(token)
+	repository_id = os.environ["GITHUB_REPOSITORY_ID"]
+	repository = github.get_repo(int(repository_id))
+
+	return {
+		"github": github,
+		"repository": repository,
+	}
+
+locals().update(setup())
+
+del setup
+
+# Add default set of imports for user scripts
+import json, os, pathlib, sys
 
 `;
 
@@ -24,7 +40,8 @@ async function main() {
 	const scriptPath = path.join(temporaryDirectory, "script.py");
 	await fsPromises.writeFile(scriptPath, script);
 	await exec.exec("pip3", ["install", "pygithub"]);
-	await exec.exec("python3", [scriptPath], {env: {"GITHUB_TOKEN": inputs.get().token}});
+	const child_env = { ...process.env, ...{"GITHUB_TOKEN": inputs.get().token} }
+	await exec.exec("python3", [scriptPath], {env: child_env});
 }
 
 main().catch(error => core.setFailed(error.stack || error));
